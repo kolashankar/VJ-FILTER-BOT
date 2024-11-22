cdf9ed4847d762606b453303e6d4fd8bec43c112
kolashankar <133280743+kolashankar@users.noreply.github.com>
2024-11-22 10:29:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002409317177'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1636733122').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002437388376').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002309517928').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '6479751412').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
